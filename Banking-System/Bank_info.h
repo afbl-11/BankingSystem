ed920d1d641a92fc13815c4c65da56579426788b
Jurell Afable <133481515+afbl-11@users.noreply.github.com>
2023-09-18 02:51:13 +0800

    trying to add new files

--- Banking-System/Bank_info.h
+++ Banking-System/Bank_info.h
@@ -6,7 +6,7 @@ private:
     double acc_balance[3] = { 1000, 2000,3000 };
     int accounts[3] = { 12345,54321 ,987654 };
     int password[3] = { 102938, 293847, 123456 };
-    double withdrawal_amount = 0, newBalance;
+    double withdrawal_amount = 0, newBalance = 0;
 public:
     void accountAccess(int acc, int pass);
     void BankMenu(int acc, double balance);
